26a0e849d538d642d7647f063cd4e15516f54560
Abhishek Kaisar <abhishekkaisar@abhisheks-macbook-pro.local>
2023-03-08 08:54:32 +0600

    adding files

---
+++ src/ParseIntString.java
@@ -0,0 +1,11 @@
+public class ParseIntString {
+    public static void main(String[] args){
+        String str = "123";
+        int num = Integer.parseInt(str);
+        System.out.println(num);
+
+
+
+
+    }
+}
